9da2f002624d0e5366eaa50336f4e4c224fddb15
Alyson Takai <shtakai@gmail.com>
2019-01-22 19:04:33 +0700

    Add simple_test package(base)

---
+++ simple_int/simple_int.go
@@ -0,0 +1,11 @@
+package simple_int
+
+func SimpleInt(i int) int{
+    //var i int
+    //suck(i)
+    return 1
+}
+
+//func suck(n int) {
+//    fmt.Printf("suck: %v", n)
+//}
---
+++ simple_int/simple_int_test.go
@@ -0,0 +1,23 @@
+package simple_int
+
+import "testing"
+
+func TestSimpleInt(t *testing.T) {
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+        {"simply not delicious", args{1} , 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SimpleInt(tt.args.i); got != tt.want {
+				t.Errorf("SimpleInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
